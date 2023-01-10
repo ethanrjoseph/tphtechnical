e51f307530eae76916a0bb8548f5f3db9f6f2a97
Ethan Joseph <ethanrj91@gmail.com>
2023-01-09 16:44:49 -0800

    first commit

---
+++ Ten_Percent_Technical_/Coordinator.swift
@@ -0,0 +1,34 @@
+//
+//  Coordinator.swift
+//  Ten_Percent_Technical_
+//
+//  Created by Ethan Joseph on 1/9/23.
+//
+
+import Foundation
+
+import UIKit
+
+protocol Coordinator {
+    var childCoordinators: [Coordinator] { get set }
+    var navigationController: UINavigationController { get set }
+
+    func start()
+}
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = ViewController()
+        vc.coordinator = self
+        navigationController.pushViewController(vc, animated: false)
+    }
+}
+
+
---
+++ Ten_Percent_Technical_/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ten_Percent_Technical_
+//
+//  Created by Ethan Joseph on 1/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    weak var coordinator: MainCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+    }
+}
